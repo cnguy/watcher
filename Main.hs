20f87e89d3b540be939ca0fbbb76bd9117b459a5
Chau <ChauTNguyen96@gmail.com>
2019-05-08 12:43:09 -0700

    Set up ping bot

--- Main.hs
+++ Main.hs
@@ -5,6 +5,7 @@ import qualified Control.Exception as E
 import Control.Lens
 import qualified Control.Monad as CM
 import qualified Data.ByteString.Char8 as B
+import Data.Char (toLower)
 import Data.List
 import Data.Monoid ((<>))
 import qualified Data.Text as T
@@ -28,6 +29,7 @@ instance ThreadSeconds Int where
 instance ThreadSeconds Double where
   loopInfinitely seconds f = loop (round $ seconds * 10 ** 6) f
 
+ping :: (String) -> IO ()
 ping url = do
   r <- Wreq.getWith opts url
   putStrLn $ url ++ ": " ++ (T.unpack $ TE.decodeUtf8 $ (r ^. Wreq.responseHeader "Date"))
@@ -35,9 +37,27 @@ ping url = do
     -- `opts` allows us to ignore suppress exceptions and grab status codes instead.
     opts = set Wreq.checkResponse (Just $ \_ _ -> return ()) Wreq.defaults
 
-main :: IO ()
+waitForDiscordPing :: (RestChan, Gateway, z) -> IO ()
+waitForDiscordPing discord = do
+    e <- nextEvent discord
+    case e of
+      Left error -> putStrLn ("Event error: " <> show error)
+      Right (MessageCreate m) -> do
+        CM.when (isPing (messageText m) && not (fromBot m)) $ do
+          resp <- restCall discord (CreateMessage (messageChannel m) "Pong!")
+          return ()
+        waitForDiscordPing discord
+      _ -> waitForDiscordPing discord
+
+fromBot :: Message -> Bool
+fromBot m = userIsBot (messageAuthor m)
+
+isPing :: T.Text -> Bool
+isPing m = (== "ping") $ (T.map toLower m)
+
 main = do
   token <- T.strip <$> TIO.readFile "./auth-token.secret"
-  discord <- loginRest (Auth token)
+  discord <- loginRestGateway (Auth token)
   let urls = ["https://zottrail.xyz/api/v1/ok", "https://zottrail.xyz/api/v1/okk"]
+  CC.forkIO $ (waitForDiscordPing discord)
   E.finally (loopInfinitely (25 :: Int) (mapM_ ping urls)) (stopDiscord discord)
