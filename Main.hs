23b61e47a26beb4eacff824c51f9cac9d226ef8b
Chau <ChauTNguyen96@gmail.com>
2019-05-08 11:42:14 -0700

    Implement adhoc polymorphism through my first typeclass

--- Main.hs
+++ Main.hs
@@ -2,8 +2,8 @@
 
 import qualified Control.Concurrent as CC
 import qualified Control.Exception as E
-import qualified Control.Monad as CM
 import Control.Lens
+import qualified Control.Monad as CM
 import qualified Data.ByteString.Char8 as B
 import Data.List
 import Data.Monoid ((<>))
@@ -15,13 +15,18 @@ import qualified Network.HTTP.Client as NHC
 import Discord
 import qualified Network.Wreq as Wreq
 
-setInterval :: Double -> IO () -> IO ()
-setInterval seconds fn = CM.forever $ do
-  fn 
-  CC.threadDelay (round $ seconds * 10 ** 6)
+loop value f = CM.forever $ do
+  f
+  CC.threadDelay value
 
---- let channelId = 562112322185723904
---- chan <- restCall discord (GetChannel channelId)
+class Num a => ThreadSeconds a where
+  loopInfinitely :: a -> IO () -> IO ()
+
+instance ThreadSeconds Int where
+  loopInfinitely seconds f = loop (seconds * 10 ^ 6) f
+
+instance ThreadSeconds Double where
+  loopInfinitely seconds f = loop (round $ seconds * 10 ** 6) f
 
 ping url = do
   r <- Wreq.getWith opts url
@@ -35,5 +40,4 @@ main = do
   token <- T.strip <$> TIO.readFile "./auth-token.secret"
   discord <- loginRest (Auth token)
   let urls = ["https://zottrail.xyz/api/v1/ok", "https://zottrail.xyz/api/v1/okk"]
-  setInterval 25.0 (mapM_ ping urls)
-  stopDiscord discord
+  E.finally (loopInfinitely (25 :: Int) (mapM_ ping urls)) (stopDiscord discord)
