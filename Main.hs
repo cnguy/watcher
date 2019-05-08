3fa93844d75d691a613b411d3fec588d97d5e7d1
Chau <ChauTNguyen96@gmail.com>
2019-05-08 15:12:15 -0700

    Send message to Twilio on death

--- Main.hs
+++ Main.hs
@@ -3,7 +3,8 @@
 import qualified Control.Concurrent as CC
 import qualified Control.Exception as E
 import Control.Lens
-import qualified Control.Monad as CM
+import Control.Monad
+import Control.Monad.IO.Class (liftIO)
 import qualified Data.ByteString.Char8 as B
 import Data.Char (toLower)
 import Data.List
@@ -12,11 +13,17 @@ import qualified Data.Text as T
 import qualified Data.Text.Encoding as TE
 import qualified Data.Text.IO as TIO
 import qualified Network.HTTP.Client as NHC
+import System.Environment (getEnv)
 
+import Configuration.Dotenv (loadFile, defaultConfig)
 import Discord
 import qualified Network.Wreq as Wreq
+import Twilio
+import Twilio.Calls as Calls
+import Twilio.Messages
 
-loop value f = CM.forever $ do
+-- | Generic, blocking loop function that runs forever.
+loop value f = forever $ do
   f
   CC.threadDelay value
 
@@ -29,6 +36,7 @@ instance ThreadSeconds Int where
 instance ThreadSeconds Double where
   loopInfinitely seconds f = loop (round $ seconds * 10 ** 6) f
 
+-- | Pings some URL without throwing an exception.
 ping :: (String) -> IO ()
 ping url = do
   r <- Wreq.getWith opts url
@@ -37,13 +45,15 @@ ping url = do
     -- `opts` allows us to ignore suppress exceptions and grab status codes instead.
     opts = set Wreq.checkResponse (Just $ \_ _ -> return ()) Wreq.defaults
 
+-- | Blocks the program and continually makes the Discord bot wait
+-- for a user to type "ping".
 waitForDiscordPing :: (RestChan, Gateway, z) -> IO ()
 waitForDiscordPing discord = do
     e <- nextEvent discord
     case e of
       Left error -> putStrLn ("Event error: " <> show error)
       Right (MessageCreate m) -> do
-        CM.when (isPing (messageText m) && not (fromBot m)) $ do
+        when (isPing (messageText m) && not (fromBot m)) $ do
           resp <- restCall discord (CreateMessage (messageChannel m) "Pong!")
           return ()
         waitForDiscordPing discord
@@ -55,9 +65,34 @@ fromBot m = userIsBot (messageAuthor m)
 isPing :: T.Text -> Bool
 isPing m = (== "ping") $ (T.map toLower m)
 
+postMessageNoOpts to from message = PostMessage to from message Nothing
+
+send to from = runTwilio' (getEnv "TWILIO_ACCOUNT_SID") (getEnv "TWILIO_AUTH_TOKEN") $ do
+  let warn = postMessageNoOpts to from
+  let body = warn "yabai"
+  message <- Twilio.Messages.post body
+  return ()
+
+getEnvAndPack :: String -> IO T.Text
+getEnvAndPack env = do
+  value <- getEnv env
+  let text = T.pack(value)
+  return text
+
+-- | Calls `send` and cleans up the Discord instance.
+-- Generally, this is used to clean up resources in the case
+-- of exceptions.
+die discord = do
+  putStrLn "die"
+  to <- getEnvAndPack "TWILIO_TO_PHONE_NUMBER"
+  from <- getEnvAndPack "TWILIO_FROM_PHONE_NUMBER"
+  send to from
+  stopDiscord discord
+
 main = do
   token <- T.strip <$> TIO.readFile "./auth-token.secret"
+  loadFile defaultConfig
   discord <- loginRestGateway (Auth token)
   let urls = ["https://zottrail.xyz/api/v1/ok", "https://zottrail.xyz/api/v1/okk"]
   CC.forkIO $ (waitForDiscordPing discord)
-  E.finally (loopInfinitely (25 :: Int) (mapM_ ping urls)) (stopDiscord discord)
+  E.finally (loopInfinitely (10 :: Int) (mapM_ ping urls)) (die discord)
